83ed8570c32e14e4dcdd6718cc639ede76f961a6
yerayherreria <yerayherre@gmail.com>
2024-01-17 10:02:34 +0100

    .

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,26 +1,32 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Minion } from '../interfaces/minion';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { MinionsService } from '../services/minions.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [CommonModule,RouterLink,RouterLinkActive,FormsModule],
+  imports: [CommonModule,RouterLink,RouterLinkActive,FormsModule,AsyncPipe],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
 export class ContentComponent implements OnInit{
   @Input() id : string = ""
-
+  minions$! : Observable<Minion[]>;
   minions2: Minion[] = [];
   constructor(private minionsService: MinionsService){}
 
   ngOnInit(): void {
-    this.minionsService.getMinions().subscribe({
-      next: (minions2) => this.minions2 = minions2
-    })
+    // this.minionsService.getMinions()
+    // .subscribe({
+    //   next: (minions2) => this.minions2 = minions2
+    // })
+    this.minions$ = this.minionsService.getMinions();
+
+    this.minionsService.getMinions()
+    .subscribe((next)=>console.log(next), (error)=>console.log(error))
   }
 
   deleteMinion(id:String){
@@ -29,7 +35,6 @@ export class ContentComponent implements OnInit{
     });
   }
 
-  //-------
   favorites: String[]=[];
   aggregate(name:String){
     this.favorites.push(name);
